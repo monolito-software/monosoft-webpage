257a1f1ad06e2ca7d9ed0e70004c372d4298bf9c
Lohana Torres <lt.leopoldo@gmail.com>
2021-06-12 17:01:34 -0300

    feat: use intro component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 
 import Header from '../components/Header';
+import Intro from '../components/Intro';
 
 import styles from '../styles/Home.module.css';
 
@@ -12,15 +13,16 @@ export default function Home() {
           name="description"
           content="Empresa especializada em soluções tecnológicas de baixos curso e alta qualidade para o seu negócio."
         />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
         <title>Monosoft</title>
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
       </Head>
-
-      <Header />
-
-      <main className={styles.main}>
-
+      <header>
+        <Header />
+      </header>
+      <main>
+        <Intro />
       </main>
     </div>
   );
