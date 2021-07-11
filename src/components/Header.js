12de6cfb19cc2eae38216d4b51fb61213e5cfeb3
Lohana Torres <lt.leopoldo@gmail.com>
2021-07-10 23:36:54 -0300

    feat: media breakpoints for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,31 +2,28 @@ import styles from '../styles/components/Header.module.css';
 
 const Header = () => {
   return (
-    <>
-      <div className={styles.headerContainer}>
-        <div className={styles.brandContainer}>
-          <img
-            src="/images/monolito.svg"
-            alt="Ilustração de um Monolito como logomarca."
-          />
-        </div>
-        <nav className={styles.linksContainer}>
-          <a href="#intro">Início</a>
-          <a href="#services">Serviços</a>
-          <a href="#products">Produtos</a>
-          <a href="#team">Quem somos</a>
-        </nav>
-        <a
-          href="mailto:contato@monosoft.com.br"
-          className={styles.contactButton}
-        >
-          <span className="button-text" id={styles.contact}>
+    <div className={styles.headerContainer}>
+      <div className={styles.brandContainer}>
+        <img
+          src='/images/monolito.svg'
+          alt='Ilustração de um Monolito como logomarca.'
+        />
+      </div>
+      <nav className={styles.linksContainer}>
+        <a href='#intro'>Início</a>
+        <a href='#services'>Serviços</a>
+        <a href='#products'>Produtos</a>
+        <a href='#team'>Quem somos</a>
+      </nav>
+      <a
+        href='mailto:contato@monosoft.com.br'
+        className={styles.contactButton}
+      >
+          <span className='button-text' id={styles.contact}>
             Contato
           </span>
-        </a>
-      </div>
-      <div className={styles.separator} />
-    </>
+      </a>
+    </div>
   );
 };
 
