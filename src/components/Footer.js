39076c0abb7c302183e825a725a20192d125a997
Lohana Torres <lt.leopoldo@gmail.com>
2021-07-10 23:36:38 -0300

    feat: media breakpoints for footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,27 +2,29 @@ import styles from '../styles/components/Footer.module.css';
 
 const Footer = () => {
   return (
-    <section className={styles.footerContainer}>
-      <div className={styles.socialMedia}>
-        <a href="https://facebook.com/monolito-software">
-          <img src="/icons/facebook.svg" alt="Ícone do Facebook" />
-        </a>
-        <a href="https://linkedin.com/in/monolito-software">
-          <img src="/icons/linkedin.svg" alt="Ícone do LinkedIn" />
-        </a>
-        <a href="https://instagram.com/monolito-software">
-          <img src="/icons/instagram.svg" alt="Ícone do Instagram" />
-        </a>
-      </div>
-      <p className={styles.copyrightText}>
-        Copyright © 2021 Monolito Software.
-      </p>
-      <a href="mailto:contato@monosoft.com.br" className={styles.contactButton}>
-        <span className="button-text">
+    <div className={styles.sectionBackground}>
+      <section className={styles.footerContainer}>
+        <div className={styles.socialMedia}>
+          <a href='https://facebook.com/monolito-software'>
+            <img src='/icons/facebook.svg' alt='Ícone do Facebook' />
+          </a>
+          <a href='https://linkedin.com/in/monolito-software'>
+            <img src='/icons/linkedin.svg' alt='Ícone do LinkedIn' />
+          </a>
+          <a href='https://instagram.com/monolito-software'>
+            <img src='/icons/instagram.svg' alt='Ícone do Instagram' />
+          </a>
+        </div>
+        <p className={styles.copyrightText}>
+          Copyright © 2021 Monolito Software.
+        </p>
+        <a href='mailto:contato@monosoft.com.br' className={styles.contactButton}>
+        <span className='button-text'>
           Contato
         </span>
-      </a>
-    </section>
+        </a>
+      </section>
+    </div>
   );
 };
 
